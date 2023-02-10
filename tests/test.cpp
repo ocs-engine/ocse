998fc377ac374ef528fd6272c50113dde096671e
Yuri Istomin <nekocoder.pw@gmail.com>
2023-02-10 20:56:28 +0200

    Isolate tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -17,23 +17,27 @@ int main() {
     std::deque<oe::Planet> planets;
 
     auto& user_1 = users.emplace_back();
-    auto& user_2 = users.emplace_back();
     auto& planet = planets.emplace_back();
 
-    "bind new planet to user_1"_test = [&] {
+    "bind new planet to user_1"_test = [=] mutable {
         oa::bind_planet_to_user(user_1, planet);
         expect(that % planet.user == &user_1);
         expect(user_1.planets.size() == 1_i);
     };
 
-    "trying bind planet to user_1 twice"_test = [&] {
+    "trying bind planet to user_1 twice"_test = [=] mutable {
+        oa::bind_planet_to_user(user_1, planet);
+
         expect(throws<std::logic_error>([&] {
             oa::bind_planet_to_user(user_1, planet);
         })) << "throws logic_error";
         expect(user_1.planets.size() == 1_i);
     };
 
-    "trying bind planet to user_2"_test = [&] {
+    "trying bind planet to user_2"_test = [=] mutable {
+        auto& user_2 = users.emplace_back();
+        oa::bind_planet_to_user(user_1, planet);
+
         expect(throws<std::runtime_error>([&] {
             oa::bind_planet_to_user(user_2, planet);
         })) << "throws runtime_error";
