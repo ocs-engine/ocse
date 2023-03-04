4bf706b8e6f867f21beb0b3035ed3194b6bae70a
Yuri Istomin <nekocoder.pw@gmail.com>
2023-03-04 14:01:30 +0200

    Rename user entity to player (#10)

--- src/actions/actions.h
+++ src/actions/actions.h
@@ -2,12 +2,12 @@
 #define OCSE_ACTIONS_H
 
 #include "entities/Planet.h"
-#include "entities/User.h"
+#include "entities/Player.h"
 
 namespace oe = ocse::entities;
 
 namespace ocse::actions {
-    void bind_planet_to_user(oe::User& user, oe::Planet& planet);
+    void bind_planet_to_player(oe::Player& player, oe::Planet& planet);
 }
 
 #endif //OCSE_ACTIONS_H
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,6 +1,6 @@
 #include "actions/actions.h"
 #include "entities/Planet.h"
-#include "entities/User.h"
+#include "entities/Player.h"
 
 #include "boost/ut.hpp"
 
@@ -13,34 +13,34 @@ namespace oa = ocse::actions;
 namespace oe = ocse::entities;
 
 int main() {
-    std::deque<oe::User> users;
+    std::deque<oe::Player> players;
     std::deque<oe::Planet> planets;
 
-    auto& user_1 = users.emplace_back();
+    auto& player_1 = players.emplace_back();
     auto& planet = planets.emplace_back();
 
-    "bind new planet to user_1"_test = [=] mutable {
-        oa::bind_planet_to_user(user_1, planet);
-        expect(that % planet.user == &user_1);
-        expect(user_1.planets.size() == 1_i);
+    "bind new planet to player_1"_test = [=] mutable {
+        oa::bind_planet_to_player(player_1, planet);
+        expect(that % planet.player == &player_1);
+        expect(player_1.planets.size() == 1_i);
     };
 
-    "trying bind planet to user_1 twice"_test = [=] mutable {
-        oa::bind_planet_to_user(user_1, planet);
+    "trying bind planet to player_1 twice"_test = [=] mutable {
+        oa::bind_planet_to_player(player_1, planet);
 
         expect(throws<std::logic_error>([&] {
-            oa::bind_planet_to_user(user_1, planet);
+            oa::bind_planet_to_player(player_1, planet);
         })) << "throws logic_error";
-        expect(user_1.planets.size() == 1_i);
+        expect(player_1.planets.size() == 1_i);
     };
 
-    "trying bind planet to user_2"_test = [=] mutable {
-        auto& user_2 = users.emplace_back();
-        oa::bind_planet_to_user(user_1, planet);
+    "trying bind planet to player_2"_test = [=] mutable {
+        auto& player_2 = players.emplace_back();
+        oa::bind_planet_to_player(player_1, planet);
 
         expect(throws<std::runtime_error>([&] {
-            oa::bind_planet_to_user(user_2, planet);
+            oa::bind_planet_to_player(player_2, planet);
         })) << "throws runtime_error";
-        expect(user_2.planets.size() == 0_i);
+        expect(player_2.planets.size() == 0_i);
     };
 }
